291cea4a23c24408f71df554973fde20312fd57f
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2024-07-08 14:55:28 +0200

    added the function close

--- mlx_init.c
+++ mlx_init.c
@@ -6,7 +6,7 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 11:26:17 by paprzyby          #+#    #+#             */
-/*   Updated: 2024/07/08 14:43:13 by paprzyby         ###   ########.fr       */
+/*   Updated: 2024/07/08 14:54:50 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main(int ac, char **av)
 	fd = open("map.ber", O_RDONLY);
 	if (fd == -1)
 	{
-		ft_printf("\nError while opening the map\n");
+		ft_printf("\nError while opening the map\n\n");
 		return (1);
 	}
 	if (ac == 2)
@@ -32,7 +32,8 @@ int	main(int ac, char **av)
 	}
 	else
 	{
-		printf("\n%s\n", get_next_line(fd));
+		ft_printf("\n%s\n\n", get_next_line(fd));
+		close(fd);
 		ft_printf("\nError:\nInvalid number of parameters\n\n");
 		return (1);
 	}
