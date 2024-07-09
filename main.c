90dfd0e28c9f3c7075f6990e6b4b6132eecc2bdb
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2024-07-09 15:24:24 +0200

    updated the function for checking the map

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 11:26:17 by paprzyby          #+#    #+#             */
-/*   Updated: 2024/07/09 09:47:12 by paprzyby         ###   ########.fr       */
+/*   Updated: 2024/07/09 15:23:59 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,38 @@
 void	read_the_map(char *map)
 {
 	char	*line;
+	char	*first_line;
 	int		fd;
+	int		width;
 
 	fd = open(map, O_RDONLY);
 	if (fd == -1)
 	{
-		ft_printf("\nError while opening the map\n\n");
+		ft_printf("Error: Problem when opening the map\n");
 		return ;
 	}
+	first_line = get_next_line(fd);
 	line = get_next_line(fd);
+	if (!line && !first_line)
+	{
+		ft_printf("Error: Invalid map\n");
+		close(fd);
+		return ;
+	}
+	width = ft_strlen(first_line);
 	while (line)
 	{
-		ft_printf("%s", line);
+		ft_printf("%s\n", line);
+		ft_printf("%d\n", width);
+		if (width != ft_strlen(line))
+		{
+			ft_printf("Error: Invalid map\n");
+			close(fd);
+			return ;
+		}
 		line = get_next_line(fd);
 	}
+	ft_printf("The map is valid\n");
 	close(fd);
 }
 
@@ -39,6 +57,6 @@ int	main(int ac, char **av)
 		read_the_map(av[1]);
 		return (0);
 	}
-	ft_printf("\nError:\nInvalid number of parameters\n\n");
+	ft_printf("Error: Invalid number of parameters\n");
 	return (1);
 }
