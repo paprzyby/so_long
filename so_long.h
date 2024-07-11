992fd1459276f247b5133d1db79d42c54ede7207
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2024-07-11 14:45:34 +0200

    included gnl and ft_printf

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 13:15:00 by paprzyby          #+#    #+#             */
-/*   Updated: 2024/07/10 15:34:15 by paprzyby         ###   ########.fr       */
+/*   Updated: 2024/07/11 14:42:19 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include "./MLX42/include/MLX42/MLX42.h"
 # include "./libft/libft.h"
+# include "./libft/ft_printf/ft_printf.h"
+# include "./libft/get_next_line/get_next_line.h"
 # include <unistd.h>
 # include <stdlib.h>
 # include <limits.h>
