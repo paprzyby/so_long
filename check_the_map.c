c07d59f7d365af3073b5743e12e0f2300ff18a9e
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2024-07-15 08:03:34 +0200

    added a function for checking the chars

--- check_the_map.c
+++ check_the_map.c
@@ -6,7 +6,7 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 16:46:36 by paprzyby          #+#    #+#             */
-/*   Updated: 2024/07/11 17:53:39 by paprzyby         ###   ########.fr       */
+/*   Updated: 2024/07/15 08:02:45 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,31 @@
 
 bool	check_the_chars(char **map)
 {
-	
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == '1')
+			{
+				return (true);
+			}
+			else if (map[i][j] == '0')
+			{
+				return (true);
+			}
+			else
+				return (false);
+			j++;
+		}
+		i++;
+	}
+	return (false);
 }
 
 bool	check_the_size(char **map)
