1c6f0d72217c57702176758d67296138be14b5b8
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2024-07-15 09:47:34 +0200

    added a new struct for the flood function

--- main.c
+++ main.c
@@ -6,11 +6,16 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 11:26:17 by paprzyby          #+#    #+#             */
-/*   Updated: 2024/07/15 09:29:27 by paprzyby         ###   ########.fr       */
+/*   Updated: 2024/07/15 09:46:47 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+bool	flood_fill(t_lst *game, t_pos *position)
+{
+	return (true);
+}
 
 bool	count_map_size(char *line, t_lst *game)
 {
@@ -24,13 +29,26 @@ bool	count_map_size(char *line, t_lst *game)
 	return (true);
 }
 
-bool	free_and_close(int fd, char *line, char *lines)
+bool	free_and_close(int fd, char *line, char *lines, bool check)
 {
-	free(line);
-	if (lines)
+	if (check == true)
+	{
+		free(line);
 		free(lines);
-	close(fd);
-	return (false);
+		close(fd);
+		return (true);
+	}
+	else if (!lines)
+	{
+		free(line);
+		close(fd);
+		return (false);
+	}
+	else
+		free(line);
+		free(lines);
+		close(fd);
+		return (false);
 }
 
 bool	read_the_map(char *map, t_lst *game)
@@ -44,27 +62,25 @@ bool	read_the_map(char *map, t_lst *game)
 		return (false);
 	line = get_next_line(fd);
 	if (!line || line[0] == '\n')
-		return (free_and_close(fd, line, lines));
+		return (free_and_close(fd, line, lines, false));
 	lines = ft_calloc(1 , 1);
 	while (line)
 	{
 		lines = ft_strjoin(lines, line);
 		if (count_map_size(line, game) == false)
-			return (free_and_close(fd, line, lines));
+			return (free_and_close(fd, line, lines, false));
 		free(line);
 		line = get_next_line(fd);
 	}
-	close(fd);
 	game->map = ft_split(lines, '\n');
 	game->map_copy = ft_split(lines, '\n');
-	free(line);
-	free(lines);
-	return (true);
+	return (!free_and_close(fd, line, lines, true));
 }
 
 int	main(int ac, char **av)
 {
 	t_lst	*game;
+	t_pos	*position;
 
 	if (ac == 2)
 	{
@@ -73,6 +89,8 @@ int	main(int ac, char **av)
 			ft_printf("Error: Invalid map\n");
 		if (check_the_map(game) == false)
 			ft_printf("Error: Invalid map\n");
+		if (flood_fill(game, position) == false)
+			ft_printf("Error: Flood fill error\n");
 		else
 			ft_printf("Valid map\n");
 		return (0);
